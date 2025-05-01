485a533f59691f46a3c5a870e35235967d4a7f21
Mauricio Herruzo <maurijavi7@gmail.com>
2025-05-02 00:24:57 +0200

    Footer Interactivo JS

--- src/home/script/main.js
+++ src/home/script/main.js
@@ -6,17 +6,21 @@ document.addEventListener("DOMContentLoaded", () =>{
     const main = document.querySelector("main");
     const header = document.querySelector("header");
     const profilePC = document.querySelector("#profilePC");
+    let profiles = [ profilePC, profile]
 
-    profile.addEventListener("click", ()=>{
-        footer.classList.remove("translate-x-full");
-        footer.classList.add("translate-x-0");
 
+    profiles.forEach(profile => {
+        profile.addEventListener("click", ()=>{
+            footer.classList.remove("translate-x-full");
+            footer.classList.add("translate-x-0");
+    
+        })
     })
 
 
     // CERRAR FOOTER
     document.addEventListener("click", (e) =>{
-        if(!footer.contains(e.target) && e.target !== profile){
+        if(!footer.contains(e.target) && (e.target !== profile && e.target !== profilePC)){
             footer.classList.add("translate-x-full");
             footer.classList.remove("translate-x-0");
 
